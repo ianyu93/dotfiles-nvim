1058ecfb9617a5dd0aa2c91ce9c296c8487832c5
Ian Yu <ian.yu@arc.com.co>
2025-08-01 03:40:27 -0400

    feat: add neogen, snippets, and outline

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -8,6 +8,29 @@ return {
 
 		neogen.setup({
 			snippet_engine = "luasnip",
+			enabled = true,
+			languages = {
+				python = {
+					template = {
+						annotation_convention = "google_docstrings", -- or "numpydoc", "reST"
+					},
+				},
+				go = {
+					template = {
+						annotation_convention = "godoc",
+					},
+				},
+				javascript = {
+					template = {
+						annotation_convention = "jsdoc",
+					},
+				},
+				typescript = {
+					template = {
+						annotation_convention = "tsdoc", -- or "jsdoc"
+					},
+				},
+			},
 		})
 
 		vim.keymap.set("n", "<leader>nf", function()
@@ -16,6 +39,10 @@ return {
 
 		vim.keymap.set("n", "<leader>nt", function()
 			neogen.generate({ type = "type" })
+		end)
+
+		vim.keymap.set("n", "<leader>nc", function()
+			neogen.generate({ type = "class" })
 		end)
 	end,
 	-- Uncomment next line if you want to follow only stable versions
