fb05130e37a00f0984498221b8d8c841f497e409
Ian Yu <ianyu@Ians-MacBook-Pro.local>
2025-12-14 00:49:22 -0500

    update: yeah too lazy

--- lua/plugins/catppucin.lua
+++ lua/plugins/catppucin.lua
@@ -4,12 +4,6 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			background = { -- :h background
-				light = "latte",
-				dark = "mocha",
-			},
-		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 
 	config = function()
 		require("lualine").setup({
-			options = { theme = "catppucin" },
+			options = { theme = "catppuccin" },
 		})
 	end,
 }
